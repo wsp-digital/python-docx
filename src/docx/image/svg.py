81569501a2deb38d648d3b221dc811e44cfb8d42
Jamie Dimos <james.dimos@wspdigital.com>
2025-03-12 16:27:50 +1100

    Handle length units that aren't pixels
    Signed-off-by: Jamie Dimos <jamiedimos@gmail.com>

--- src/docx/image/svg.py
+++ src/docx/image/svg.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import xml.etree.ElementTree as ET
 
 from .constants import MIME_TYPE
@@ -10,7 +11,7 @@ class Svg(BaseImageHeader):
     """
 
     @classmethod
-    def from_stream(cls, stream):
+    def from_stream(cls, stream: BytesIO):
         """
         Return |Svg| instance having header properties parsed from SVG image
         in *stream*.
@@ -34,12 +35,36 @@ class Svg(BaseImageHeader):
         return "svg"
 
     @classmethod
-    def _dimensions_from_stream(cls, stream):
+    def _length_to_px(cls, length: str):
+        """
+        Convert SVG length unit to pixels.
+
+        See the table of absolute length units in W3 CSS Values 3
+        https://www.w3.org/TR/css-values-3/#absolute-lengths
+        """
+        s = str(length)
+        unit = s.lstrip("0123456789.").casefold()
+        value_string = s[: -len(unit)] if len(unit) > 0 else s
+        value = float(value_string)
+        inch = 96
+        cm = inch / 2.54
+        units = {
+            "cm": cm,
+            "mm": cm / 10,
+            "q": cm / 40,
+            "in": inch,
+            "pc": inch / 6,
+            "pt": inch / 72,
+            "px": 1,
+        }
+        px = value * units.get(unit, 1)
+        return int(px)
+
+    @classmethod
+    def _dimensions_from_stream(cls, stream: BytesIO):
         stream.seek(0)
         data = stream.read()
         root = ET.fromstring(data)
-        # FIXME: The width could be expressed as '4cm'
-        # See https://www.w3.org/TR/SVG11/struct.html#NewDocument
-        width = int(root.attrib["width"])
-        height = int(root.attrib["height"])
+        width = cls._length_to_px(root.attrib["width"])
+        height = cls._length_to_px(root.attrib["height"])
         return width, height
